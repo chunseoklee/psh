51a67ab2b209ad1e44b9a0549c2a1eaff564eed3
twoflower <chunseoklee@naver.com>
2015-05-08 00:04:36 +0900

    psh13

--- psh13/A.scala
+++ psh13/A.scala
@@ -1,7 +1,31 @@
 object A {
+  def apo[A, B](v: B)(f: B => Option[(A, Either[B, List[A]])]): List[A] = f(v) match {
+   case None => Nil
+   case Some((a, Left(b)))   => a :: apo(b)(f)
+   case Some((a, Right(as))) => a :: as 
+}
+
+  def interleave[A](period: Int, substitutes: List[A], elems: List[A]): List[A] =
+    apo((period, substitutes, elems)){
+      case (_, _, Nil)       => None
+      case (_, Nil, v :: vs) => Some((v, Right(vs)))
+      case (0, x :: xs, vs)  => Some((x, Left((period, xs, vs))))
+      case (n, xs, v :: vs)  => Some((v, Left((n - 1, xs, vs))))  
+    }
+
   def main(args: Array[String]) = {
-    val a = readLine.split(" ");
-    val b = a.map(_.toInt).toList;
-    b.reverse.foreach(println);
+    val input = readLine.split(" ").map(_.toInt).toList;
+    val n = input(0);
+    val m = input(1);
+    val l = (for (i <- 1 to m) yield i).toList; 
+    val (first,rest) = l.splitAt(m/2);
+    val order = if (m%2 == 0) {
+      interleave(1,(first.reverse) , rest);
+    } 
+    else {
+      rest.head::interleave(1,first.reverse , rest.tail);
+    }
+    
+    println((order));
   }
 }
