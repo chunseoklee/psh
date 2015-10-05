f18e893d4bac94d4b5033f8a3756a90130424e5b
chunseok lee <chunseok.lee@samsung.com>
2015-10-05 15:02:54 +0900

    psh 26 added

--- psh20/B.rb
+++ psh20/B.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
-n = gets
-nums = gets.scan(/\d+/).map(&:to_i).sort
-for i in 0..(nums.length-1)
-  if nums[i] > 5 then
-    break
-  end
-  puts nums.at(i)
+#n = gets
+##nums = gets.scan(/\d+/).map(&:to_i).sort
+for i in 0..(160000)
+#  if nums[i] > 5 then
+#    break
+#  end
+  puts 1
 end
 #print nums
 #print "\n"
