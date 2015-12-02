51077b703a9dc5429c33e0e664ccb9db467473ff
chunseok lee <chunseok.lee@samsung.com>
2015-12-02 17:21:24 +0900

    .

--- psh33/B.cpp
+++ psh33/B.cpp
@@ -1,34 +1,129 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
-
+#include <set>
+#include <algorithm>
 
 using namespace std;
 #define MAX 5001
 
 int N;
-int i,j;
 vector<int> e[MAX];
 vector<int> te[MAX];
+vector<int> child[MAX];
+set<int> comb;
 
 int s,t;
 bool visited[MAX] = {false,};
+int parent[MAX];
 
-int subafter1c[MAX] = {0,};
 
 void make_tree(int index) {
-  printf("make tree visit %d\n",index);
   visited[index] = true;
 
-  for (i = 0; i < e[index].size(); i++) {
-    if (visited[e[index][i]] == false) {
-      te[index].push_back(e[index][i]);
-      make_tree(e[index][i]);
+  for (int i = 0; i < e[index].size(); i++) {
+    int next = e[index][i];
+    if (visited[next] == false) {
+      te[index].push_back(next);
+      parent[next] = index;
+      make_tree(next);
     }
   }
 }
 
 
+int count_child(int index) {
+  int sum = 1;
+
+  for (int i = 0; i < te[index].size(); i++) {
+    int next = te[index][i];
+    int temp = count_child(next);
+    //printf("index: %d %d-th child num : %d\n", index, i ,temp);
+    sum+=temp;
+    child[index].push_back(temp);
+  }
+
+  return sum;
+}
+
+bool possible[MAX][MAX];
+
+void compute_comb(vector<int> com)
+{
+
+  set<int> result;
+  int sum = 0;
+
+  int com_ar[MAX];
+
+  sort(com.begin(),com.end());
+
+  for (int i = 0; i <= N; i++) {
+    for (int j = 0; j <= com.size(); j++) {
+      possible[i][j] = false;
+    }
+  }
+
+  for (int j = 0; j < com.size(); j++) {
+    com_ar[j+1] = com[j];
+    //cout << "..."<< com[j] << ":";
+    possible[com[j]][j+1] = true;
+    sum += com[j];
+  }
+  //  cout << "sum " << sum << endl;
+  for (int i = 1; i <= sum-1; i++) {
+    for (int j = 1; j <= (com.size()); j++) {
+      possible[i][j] = ( possible[i][j] || possible[i][j-1] );
+      if (com_ar[j] <= i && !possible[i][j]){
+        possible[i][j] = possible[i-com_ar[j]][j-1];
+      }
+      //printf("%d ",possible[i][j]);
+    }
+    //cout << endl;
+  }
+
+  for (int i = 1; i <= sum-1; i++) {
+    if (possible[i][com.size()]) {
+      //printf("possible:%d \n",i);
+      comb.insert(i);
+    }
+  }
+}
+
+
+void solve()
+{
+
+  for (int i = 1; i <= N; i++) {
+    vector<int> com;
+    //printf("node %d child: %d parent: %d\n",i, child[i].size(),parent[i]);
+    if (child[i].size() > 0 && parent[i] != 0) {
+      int sum = 0;
+      for (int j = 0; j < child[i].size(); j++) {
+        sum += child[i][j];
+        com.push_back(child[i][j]);
+        //cout << "com pushed:" << child[i][j] << endl;
+      }
+      if (i != 1) {
+        com.push_back(N-1-sum);
+        //cout << "com pushed:" << (N-1-sum) << endl;
+      }
+
+      if (com.size() == 1){
+      }
+      else if (com.size() == 2) {
+        comb.insert(com[0]);
+        comb.insert(com[1]);
+      } else {
+        //printf("comb");
+        compute_comb(com);
+      }
+    }
+  }
+
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -36,15 +131,27 @@ int main(int argc, char *argv[])
   cin >> N;
 
 
-  for (i = 0; i < N; i++) {
+  for (int i = 0; i < N-1; i++) {
 
     cin >> s >> t;
-    //cout << "read" << i << ":" << s << t << endl;
     e[s].push_back(t);
     e[t].push_back(s);
+
   }
 
+  parent[1] = 1;
   make_tree(1);
+
+  count_child(1);
+
+
+  solve();
+
+  cout << comb.size() << endl;
+  for( set<int>::iterator IterPos = comb.begin();
+       IterPos != comb.end(); ++IterPos ) {
+    cout << *IterPos << " " << N-1-(*IterPos) << endl;
+  }
 
   return 0;
 }
