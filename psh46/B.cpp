fb80a8b60aaa1834853ba4599a6d5718aad64759
chunseok lee <chunseok.lee@samsung.com>
2016-03-30 15:55:39 +0900

    .

--- psh46/B.cpp
+++ psh46/B.cpp
@@ -9,45 +9,88 @@ using namespace std;
 LL N,M;
 LL _N,_M;
 
-LL l[MAX],ql[MAX];
-char c[MAX],qc[MAX];
+LL l[MAX],patternl[MAX];
+char text[MAX],pattern[MAX];
+
+char overlap[MAX];
+//char pattern[MAX];
+
 
 void print_input() {
- for (LL i=1; i<=N; i++)
-  printf("%lld-%c\n",l[i],c[i]);
+ for (LL i=0; i<N; i++)
+  printf("%lld-%c\n",l[i],text[i]);
 
- for (LL i=1; i<=M; i++)
-  printf("q:%lld-%c\n",ql[i],qc[i]);
+ for (LL i=0; i<M; i++)
+  printf("q:%lld-%c\n",patternl[i],pattern[i]);
 
 }
+
+
+
+int KMP()
+{
+  int matched_count = 0;
+
+  int j = 0;
+  for (int i = 0; i < N; i++)
+    for (;;) {      // loop until break
+      if (text[i] == pattern[j]) { // matches?
+        j++;        // yes, move on to next state
+        if (j == M) {   // maybe that was the last state
+          matched_count++;
+          j = overlap[j];
+        }
+        break;
+      } else if (j == 0) break;   // no match in state j=0, give up
+      else j = overlap[j];    // try shorter partial match
+    }
+
+  return matched_count;
+}
+
+void compute_overlap() {
+    overlap[0] = -1;
+    for (int i = 0; pattern[i] != '\0'; i++) {
+      overlap[i + 1] = overlap[i] + 1;
+      while (overlap[i + 1] > 0 &&
+             (pattern[i] != pattern[overlap[i + 1] - 1]
+              || patternl[i] != patternl[overlap[i+1] -1]))
+        overlap[i + 1] = overlap[overlap[i + 1] - 1] + 1;
+    }
+}
+
+
 
 int main() {
   // 1. read input
   cin >> _N >> _M;
-  N = 1;
-  M = 1;
-  for (LL i=1; i<=_N; i++) {
-    scanf("%lld-%c",&l[N],&c[N]);
-    if ( i != 1 ) {
-      if (c[N-1] == c[N])  {
+  N = 0;
+  M = 0;
+  for (LL i=0; i<_N; i++) {
+    scanf("%lld-%c",&l[N],&text[N]);
+    if ( i != 0 ) {
+      if (text[N-1] == text[N])  {
         l[N-1] += l[N]; continue;
       }
     }
     N++;
   }
-  N--;
+  //N--;
 
-  for (LL i=1; i<=_M; i++) {
-    scanf("%lld-%c",&ql[M],&qc[M]);
-    if ( i != 1 ) {
-      if (c[M-1] == c[M])  {
-        l[M-1] += l[M]; continue;
+  for (LL i=0; i<_M; i++) {
+    scanf("%lld-%c",&patternl[M],&pattern[M]);
+    if ( i != 0 ) {
+      if (pattern[M-1] == pattern[M])  {
+        patternl[M-1] += patternl[M]; continue;
       }
     }
     M++;
   }
-  M--;
+  //M--;
   print_input();
+
+  compute_overlap();
+  cout << "answer:" << KMP() << endl;
 
   return 0;
 }
